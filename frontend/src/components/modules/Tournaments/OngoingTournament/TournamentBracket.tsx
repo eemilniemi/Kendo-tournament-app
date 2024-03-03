e44139c40ff26e7c72602c280d7f1b11b20c5c7d
tktstudent <mikaelkonu10@gmail.com>
2024-03-03 18:46:03 +0200

    add playoff result

--- frontend/src/components/modules/Tournaments/OngoingTournament/TournamentBracket.tsx
+++ frontend/src/components/modules/Tournaments/OngoingTournament/TournamentBracket.tsx
@@ -88,6 +88,17 @@ const Bracket: React.FC<BracketProps> = ({ match, players }) => {
             <Typography textAlign="center" style={{ color: player2Color }}>
               {player2Name}
             </Typography>
+            {isWinnerDeclared && (
+              <Typography textAlign="center" variant="h6">
+                <span style={{ color: player1Color }}>
+                  {match.player1Score}
+                </span>{" "}
+                -{" "}
+                <span style={{ color: player2Color }}>
+                  {match.player2Score}
+                </span>
+              </Typography>
+            )}
             {match.elapsedTime <= 0 &&
               officialsInfo.map((info, index) => (
                 <Typography textAlign="center" key={index} variant="body2">
