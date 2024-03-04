5a9e4dd8375ab02e848feef75f12d67e7fd3a98e
nhpiro <pinja.rontu@tuni.fi>
2024-03-04 20:35:49 +0200

    Add translations to terms and conditions

--- frontend/src/components/modules/Legal/TermsAndConditions.tsx
+++ frontend/src/components/modules/Legal/TermsAndConditions.tsx
@@ -2,228 +2,137 @@ import React from "react";
 import { Typography, Link } from "@mui/material";
 import routePaths from "routes/route-paths";
 import { Link as RouterLink } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 const TermsAndConditions: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <div className="Terms">
-      <Typography variant="h2">TERMS AND CONDITIONS</Typography>
-      <Typography variant="subtitle1">Last updated March 4, 2024</Typography>
-      <Typography variant="h3">AGREEMENT TO OUR LEGAL TERMS</Typography>
+      <Typography variant="h2">{t("terms_and_conditions.title")}</Typography>
+      <Typography variant="subtitle1">
+        {t("terms_and_conditions.last_updated")}
+      </Typography>
+      <Typography variant="h3">{t("terms_and_conditions.title2")}</Typography>
       <Typography variant="body1">
-        We are Sei Shin Ken Kai ry (&apos;Company&apos;, &apos;we&apos;,
-        &apos;us&apos;, or &apos;our&apos;). We operate the website{" "}
-        <Link href="app.kendoliiga.fi">app.kendoliiga.fi</Link> (the
-        &apos;Site&apos;), as well as any other related products and services
-        that refer or link to these legal terms (the &apos;Legal Terms&apos;)
-        (collectively, the &apos;Services&apos;).
+        {t("terms_and_conditions.we")}{" "}
+        <Link href="app.kendoliiga.fi">app.kendoliiga.fi</Link>{" "}
+        {t("terms_and_conditions.we2")}
       </Typography>
       <br></br>
-      <Typography variant="h4">1. OUR SERVICES</Typography>
+      <Typography variant="h4">
+        1. {t("terms_and_conditions.title3")}
+      </Typography>
+      <Typography variant="body1">{t("terms_and_conditions.body1")}</Typography>
+      <br></br>
+      <Typography variant="h4">
+        2. {t("terms_and_conditions.title4")}
+      </Typography>
       <Typography variant="body1">
-        The information provided when using the Services is not intended for
-        distribution to or use by any person or entity in any jurisdiction or
-        country where such distribution or use would be contrary to law or
-        regulation or which would subject us to any registration requirement
-        within such jurisdiction or country. Accordingly, those persons who
-        choose to access the Services from other locations do so on their own
-        initiative and are solely responsible for compliance with local laws, if
-        and to the extent local laws are applicable.
+        {t("terms_and_conditions.body2")}
+        <br></br>
+        <br></br>
+        {t("terms_and_conditions.body3")}
       </Typography>
       <br></br>
-      <Typography variant="h4">2. INTELLECTUAL PROPERTY RIGHTS</Typography>
+      <Typography variant="h4">
+        3. {t("terms_and_conditions.title5")}
+      </Typography>
       <Typography variant="body1">
-        We are the owner of the license of all intellectual property rights in
-        our Services, including all source code, databases, functionality,
-        software, website designs, audio, video, text, photographs, and graphics
-        in the Services (collectively, the &apos;Content&apos;), as well as the
-        trademarks, service marks, and logos contained (therein the
-        &apos;Marks&apos;).
+        {t("terms_and_conditions.body4")}
+        <br></br>
+        {t("terms_and_conditions.body5")}
         <br></br>
         <br></br>
-        Any breach of these Intellectual Property Rights will constitute a
-        material breach of our Legal Terms and your right to use our Services
-        will terminate immediately.
+        {t("terms_and_conditions.body6")}
       </Typography>
       <br></br>
-      <Typography variant="h4">3. USER REPRESENTATIONS</Typography>
+      <Typography variant="h4">
+        4. {t("terms_and_conditions.title6")}
+      </Typography>
+      <Typography variant="body1">{t("terms_and_conditions.body7")}</Typography>
+      <br></br>
+      <Typography variant="h4">
+        5. {t("terms_and_conditions.title7")}
+      </Typography>
       <Typography variant="body1">
-        By using the Services, you represent and warrant that:
-        <br></br>
-        All registration information you submit will be true, accurate, current,
-        and complete; you will maintain the accuracy of such information and
-        promptly update such registration information as necessary; you have the
-        legal capacity and you agree to comply with these Legal Terms; you are
-        not a minor in the jurisdiction in which you reside; you will not access
-        the Services through automated or non-human means, whether through a
-        bot, script or otherwise; you will not use the Services for any illegal
-        or unauthorised purpose; and your use of the Services will not violate
-        any applicable law or regulation.
+        {t("terms_and_conditions.body8")}
         <br></br>
         <br></br>
-        If you provide any information that is untrue, inaccurate, not current,
-        or incomplete, we have the right to suspend or terminate your account
-        and refuse any and all current or future use of the Services (or any
-        portion thereof).
+        {t("terms_and_conditions.body9")}
+        <br></br>
+        <br></br>
+        {t("terms_and_conditions.body10")}
+        <br></br>
+        <br></br>
+        {t("terms_and_conditions.body11")}
+        <br></br>
+        {t("terms_and_conditions.body12")}
+        <br></br>
+        {t("terms_and_conditions.body13")}
+        <br></br>
+        {t("terms_and_conditions.body14")}
+        <br></br>
+        {t("terms_and_conditions.body15")}
+        <br></br>
+        {t("terms_and_conditions.body16")}
+        <br></br>
+        {t("terms_and_conditions.body17")}
+        <br></br>
+        {t("terms_and_conditions.body18")}
+        <br></br>
+        {t("terms_and_conditions.body19")}
+        <br></br>
+        {t("terms_and_conditions.body20")}
+        <br></br>
+        {t("terms_and_conditions.body21")}
+        <br></br>
+        {t("terms_and_conditions.body22")}
       </Typography>
       <br></br>
-      <Typography variant="h4">4. USER REGISTRATION</Typography>
-      <Typography variant="body1">
-        You may be required to register to use the Services. You agree to keep
-        your password confidential and will be responsible for all use of your
-        account and password. We reserve the right to remove, reclaim, or change
-        a username you select if we determine, in our sole discretion, that such
-        username is inappropriate, obscene, or otherwise objectionable.
+      <Typography variant="h4">
+        6. {t("terms_and_conditions.title8")}
       </Typography>
-      <br></br>
-      <Typography variant="h4">5. PROHIBITED ACTIVITIES</Typography>
       <Typography variant="body1">
-        Except as set out in this section or elsewhere in our Legal Terms, no
-        part of the Services and no Content or Marks may be copied, reproduced,
-        aggregated, republished, uploaded, posted, publicly displayed, encoded,
-        translated, transmitted, distributed, sold, licensed, or otherwise
-        exploited for any commercial purpose whatsoever, without our express
-        prior written permission.
+        {t("terms_and_conditions.body23")}
+        <br></br>
+        (1) {t("terms_and_conditions.body24")}
+        <br></br>
+        (2) {t("terms_and_conditions.body25")}
+        <br></br>
+        (3) {t("terms_and_conditions.body26")}
+        <br></br>
+        (4) {t("terms_and_conditions.body27")}
+        <br></br>
+        (5) {t("terms_and_conditions.body28")}
         <br></br>
         <br></br>
-        It is prohibited to post, send, publish, upload, or transmit through the
-        Services any Submission that is illegal, harassing, hateful, harmful,
-        defamatory, obscene, bullying, abusive, discriminatory, threatening to
-        any person or group, sexually explicit, false, inaccurate, deceitful, or
-        misleading; to the extent permissible by applicable law, waive any and
-        all moral rights to any such Submission.
-        <br></br>
-        <br></br>
-        You are solely responsible for your Submissions and you expressly agree
-        to reimburse us for any and all losses that we may suffer because of
-        your breach of (a) this section, (b) any third party’s intellectual
-        property rights, or (c) applicable law.
-        <br></br>
-        <br></br>
-        As a user of the Services, you agree not to:
-        <br></br>
-        Upload or transmit (or attempt to upload or to transmit) viruses, Trojan
-        horses, or other material, including excessive use of capital letters
-        and spamming (continuous posting of repetitive text), that interferes
-        with any party’s uninterrupted use and enjoyment of the Services or
-        modifies, impairs, disrupts, alters, or interferes with the use,
-        features, functions, operation, or maintenance of the Services.
-        <br></br>
-        Systematically retrieve data or other content from the Services to
-        create or compile, directly or indirectly, a collection, compilation,
-        database, or directory without written permission from us.
-        <br></br>
-        Trick, defraud, or mislead us and other users, especially in any attempt
-        to learn sensitive account information such as user passwords.
-        <br></br>
-        Circumvent, disable, or otherwise interfere with security-related
-        features of the Services, including features that prevent or restrict
-        the use or copying of any Content or enforce limitations on the use of
-        the Services and/or the Content contained therein.
-        <br></br>
-        Disparage, tarnish, or otherwise harm, in our opinion, us and/or the
-        Services.
-        <br></br>
-        Use any information obtained from the Services in order to harass,
-        abuse, or harm another person.
-        <br></br>
-        Engage in any automated use of the system, such as using scripts to send
-        comments or messages, or using any data mining, robots, or similar data
-        gathering and extraction tools.
-        <br></br>
-        Attempt to impersonate another user or person or use the username of
-        another user.
-        <br></br>
-        Interfere with, disrupt, or create an undue burden on the Services or
-        the networks or services connected to the Services.
-        <br></br>
-        Attempt to bypass any measures of the Services designed to prevent or
-        restrict access to the Services, or any portion of the Services.
-        <br></br>
-        Make any unauthorised use of the Services, including collecting
-        usernames and/or email addresses of users by electronic or other means
-        for the purpose of sending unsolicited email, or creating user accounts
-        by automated means or under false pretences.
-      </Typography>
-      <br></br>
-      <Typography variant="h4">6. SERVICES MANAGEMENT</Typography>
-      <Typography variant="body1">
-        We reserve the right, but not the obligation, to:
-        <br></br>
-        (1) monitor the Services for violations of these Legal Terms;
-        <br></br>
-        (2) take appropriate legal action against anyone who, in our sole
-        discretion, violates the law or these Legal Terms, including without
-        limitation, reporting such user to law enforcement authorities;
-        <br></br>
-        (3) in our sole discretion and without limitation, refuse, restrict
-        access to, limit the availability of, or disable (to the extent
-        technologically feasible) any of your Contributions or any portion
-        thereof;
-        <br></br>
-        (4) in our sole discretion and without limitation, notice, or liability,
-        to remove from the Services or otherwise disable all files and content
-        that are excessive in size or are in any way burdensome to our systems;
-        and
-        <br></br>
-        (5) otherwise manage the Services in a manner designed to protect our
-        rights and property and to facilitate the proper functioning of the
-        Services.
-        <br></br>
-        <br></br>
-        We care about data privacy and security. Please review our{" "}
+        {t("terms_and_conditions.body29")}{" "}
         <Link component={RouterLink} to={routePaths.privacy}>
-          Privacy Policy
+          {t("navigation.privacy_policy")}
         </Link>
-        . By using the Services, you agree to be bound by our Privacy Policy,
-        which is incorporated into these Legal Terms.
+        {t("terms_and_conditions.body30")}
         <br></br>
         <br></br>
-        Please be advised the Services are hosted Finland. If you access the
-        Services from any other region of the world with laws or other
-        requirements governing personal data collection, use, or disclosure that
-        differ from applicable laws in Finland, then through your continued use
-        of the Services, you are transferring your data to Finland and you
-        expressly consent to have your data transferred to and processed in
-        Finland.
+        {t("terms_and_conditions.body31")}
       </Typography>
       <br></br>
-      <Typography variant="h4">7. MODIFICATIONS AND INTERRUPTIONS</Typography>
+      <Typography variant="h4">
+        7. {t("terms_and_conditions.title9")}
+      </Typography>
       <Typography variant="body1">
-        We reserve the right to change, modify, or remove the contents of the
-        Services at any time or for any reason at our sole discretion without
-        notice. However, we have no obligation to update any information on our
-        Services. We will not be liable to you or any third party for any
-        modification, price change, suspension, or discontinuance of the
-        Services. We cannot guarantee the Services will be available at all
-        times. We may experience hardware, software, or other problems or need
-        to perform maintenance related to the Services, resulting in
-        interruptions, delays, or errors. We reserve the right to change,
-        revise, update, suspend, discontinue, or otherwise modify the Services
-        at any time or for any reason without notice to you. You agree that we
-        have no liability whatsoever for any loss, damage, or inconvenience
-        caused by your inability to access or use the Services during any
-        downtime or discontinuance of the Services. Nothing in these Legal Terms
-        will be construed to obligate us to maintain and support the Services or
-        to supply any corrections, updates, or releases in connection therewith.
+        {t("terms_and_conditions.body32")}
       </Typography>
       <br></br>
-      <Typography variant="h4">8. USER DATA</Typography>
+      <Typography variant="h4">
+        8. {t("terms_and_conditions.title10")}
+      </Typography>
       <Typography variant="body1">
-        We will maintain certain data that you transmit to the Services for the
-        purpose of managing the performance of the Services, as well as data
-        relating to your use of the Services. Although we perform regular
-        routine backups of data, you are solely responsible for all data that
-        you transmit or that relates to any activity you have undertaken using
-        the Services. You agree that we shall have no liability to you for any
-        loss or corruption of any such data, and you hereby waive any right of
-        action against us arising from any such loss or corruption of such data.
+        {t("terms_and_conditions.body33")}
       </Typography>
       <br></br>
       <br></br>
       <Typography variant="subtitle1">
-        In order to resolve a complaint regarding the Services or to receive
-        further information regarding use of the Services, please contact us at:
-        info(at)kendoseinajoki.fi
+        {t("terms_and_conditions.body34")} info(at)kendoseinajoki.fi
       </Typography>
     </div>
   );
