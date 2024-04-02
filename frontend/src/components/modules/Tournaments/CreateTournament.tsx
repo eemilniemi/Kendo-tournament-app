b3023241c9c70383b2582191c9acf7b59d5d4ca5
Pinja Rontu <pinski2001@gmail.com>
2024-04-02 13:09:23 +0300

    Fix time picking in tournament creation

--- frontend/src/components/modules/Tournaments/CreateTournament.tsx
+++ frontend/src/components/modules/Tournaments/CreateTournament.tsx
@@ -20,7 +20,8 @@ import {
   DialogContent,
   DialogTitle,
   Stack,
-  Box
+  Box,
+  useMediaQuery
 } from "@mui/material";
 import updateLocale from "dayjs/plugin/updateLocale";
 
@@ -94,6 +95,7 @@ const CreateTournamentForm: React.FC = () => {
   const { differentOrganizer, startDate, type, paid } =
     useWatch<CreateTournamentFormData>(formContext);
   const [isConfirmationDialogOpen, setConfirmationDialogOpen] = useState(false);
+  const mobile = useMediaQuery("(max-width:600px)");
 
   const onSubmit = async (data: CreateTournamentFormData): Promise<void> => {
     try {
@@ -196,11 +198,14 @@ const CreateTournamentForm: React.FC = () => {
             minDateTime={now}
             format="DD/MM/YYYY HH:mm"
             ampm={false}
-            viewRenderers={{
-              hours: null,
-              minutes: null,
-              seconds: null
-            }}
+            {...(!mobile && {
+              // Only text input on desktop
+              viewRenderers: {
+                hours: null,
+                minutes: null,
+                seconds: null
+              }
+            })}
           />
           <DateTimePickerElement
             required
@@ -209,11 +214,14 @@ const CreateTournamentForm: React.FC = () => {
             minDateTime={startDate}
             format="DD/MM/YYYY HH:mm"
             ampm={false}
-            viewRenderers={{
-              hours: null,
-              minutes: null,
-              seconds: null
-            }}
+            {...(!mobile && {
+              // Only text input on desktop
+              viewRenderers: {
+                hours: null,
+                minutes: null,
+                seconds: null
+              }
+            })}
           />
         </Stack>
 
