25611208dba49ee96cd273a076dbab0abcad3d56
Mikael Konu <mikaelk@Mikael-MacBook-Pro.local>
2024-04-15 18:48:29 +0300

    add court number to playoff

--- frontend/src/components/modules/Tournaments/OngoingTournament/PlayoffTournamentView.tsx
+++ frontend/src/components/modules/Tournaments/OngoingTournament/PlayoffTournamentView.tsx
@@ -144,11 +144,18 @@ const PlayoffTournamentView: React.FC = () => {
                         }
                       );
                       return (
-                        <Bracket
-                          key={match.id}
-                          players={tempPlayers}
-                          match={match}
-                        />
+                        <Grid item>
+                          <Typography variant="body2" style={{ textAlign: "center" }}>
+                            {t("tournament_view_labels.court_number")}
+                            {": "}
+                            {match.courtNumber}
+                          </Typography>
+                          <Bracket
+                            key={match.id}
+                            players={tempPlayers}
+                            match={match}
+                          />
+                        </Grid>
                       );
                     })}
                   </Box>
