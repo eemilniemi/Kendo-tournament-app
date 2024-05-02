1d34804e62052ac45e0a7c483541992749e22141
Henri Rantala <henri.r.rantala@tuni.fi>
2024-05-02 15:15:17 +0300

    lint fix

--- frontend/src/components/modules/Tournaments/OngoingTournament/PremPlayoff/PreliminaryPlayoffView.tsx
+++ frontend/src/components/modules/Tournaments/OngoingTournament/PremPlayoff/PreliminaryPlayoffView.tsx
@@ -362,7 +362,8 @@ const PreliminaryPlayoffView: React.FC = () => {
           {allMatchesPlayed(tournamentData) && (
             <Typography variant="subtitle1">
               <span>
-                {t("frontpage_labels.winner")}{": "}
+                {t("frontpage_labels.winner")}
+                {": "}
                 {findTournamentWinner(tournamentData)}
               </span>
             </Typography>
