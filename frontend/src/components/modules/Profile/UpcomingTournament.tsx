a05a5ac55e63e186fcea64363c50e9815e979eea
Hoang Vu Hieu <hieu.hoang@tuni.fi>
2024-11-17 05:17:19 +0200

    feat: implemented upcoming tournaments tab

--- frontend/src/components/modules/Profile/UpcomingTournament.tsx
+++ frontend/src/components/modules/Profile/UpcomingTournament.tsx
@@ -4,16 +4,18 @@ import {
   Table,
   TableCell,
   TableContainer,
-  Paper,
-  Box
+  Box,
+  Button
 } from "@mui/material";
 import api from "api/axios";
 import { useAuth } from "context/AuthContext";
-import type { Tournament } from "types/models";
+import type { Tournament, User } from "types/models";
+import { useNavigate } from "react-router-dom";
 
 const UpcomingTournament: React.FC = () => {
   const { userId } = useAuth();
   const [tournaments, setTournaments] = useState<Tournament[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchTournaments = async (): Promise<void> => {
@@ -38,23 +40,109 @@ const UpcomingTournament: React.FC = () => {
     void fetchTournaments();
   }, [userId]);
 
+  const getPlayerNameById = (players: User[], playerId: string): string => {
+    if (playerId === userId) {
+      return "You";
+    }
+    const player = players.find((player) => player.id === playerId);
+    if (player != null) {
+      return `${player.firstName} ${player.lastName}`;
+    } else {
+      return "Unknown Player";
+    }
+  };
+
   return (
     <>
-      <Typography>My upcoming tournaments</Typography>
+      <Typography sx={{ mt: 2, fontSize: 35 }}>
+        My upcoming tournaments
+      </Typography>
       {tournaments.map((tournament) => {
         return (
           <Box key={tournament.id}>
-            <TableContainer component={Paper}>
-              <Table sx={{ minWidth: 650 }}>
-                <TableCell>{tournament.name}</TableCell>
-                <TableCell>{tournament.category}</TableCell>
-                <TableCell>
-                  {new Date(tournament.startDate).toLocaleDateString("fi-FI")}
-                </TableCell>
-                <TableCell>{tournament.location}</TableCell>
-              </Table>
-            </TableContainer>
-            <Typography>Known matches</Typography>
+            <Box sx={{ display: "flex", justifyContent: "flex-end", mt: 2 }}>
+              <TableContainer>
+                <Table
+                  sx={{
+                    minWidth: 500,
+                    fontSize: 20,
+                    "& .MuiTableCell-root": {
+                      borderLeft: "2px solid transparent",
+                      borderRight: "2px solid transparent",
+                      borderTop: "2px solid transparent",
+                      borderBottom: "2px solid black"
+                    }
+                  }}
+                  aria-label="simple table"
+                >
+                  <TableCell sx={{ fontSize: 20, fontWeight: "bold" }}>
+                    {tournament.name}
+                  </TableCell>
+                  <TableCell>
+                    {tournament.category.charAt(0).toUpperCase() +
+                      tournament.category.slice(1)}
+                  </TableCell>
+                  <TableCell>
+                    {new Date(tournament.startDate).toLocaleDateString("fi-FI")}{" "}
+                    - {new Date(tournament.endDate).toLocaleDateString("fi-FI")}
+                  </TableCell>
+                  <TableCell>{tournament.location}</TableCell>
+                </Table>
+              </TableContainer>
+              <Button
+                variant="contained"
+                color="primary"
+                sx={{
+                  margin: 2,
+                  minHeight: 45,
+                  fontSize: 10,
+                  minWidth: 150,
+                  borderRadius: 10
+                }}
+                onClick={() => {
+                  navigate(`/tournaments/${tournament.id}`);
+                }}
+              >
+                Tournament details
+              </Button>
+            </Box>
+            <Typography sx={{ mt: 2 }}>Known matches</Typography>
+            {tournament.matchSchedule
+              .filter((match) =>
+                match.players.some((player) => player.id === userId)
+              )
+              .map((match) => {
+                // Ensure the name of the current user always comes first
+                const sortedPlayers = [...match.players].sort((a, b) =>
+                  a.id === userId ? -1 : b.id === userId ? 1 : 0
+                );
+
+                return (
+                  <Box
+                    key={match.id}
+                    sx={{
+                      mt: 2,
+                      border: "1px solid black",
+                      padding: 2,
+                      maxWidth: 250
+                    }}
+                  >
+                    <Box sx={{ alignItems: "center" }}>
+                      <Typography>
+                        {getPlayerNameById(
+                          tournament.players,
+                          sortedPlayers[0].id
+                        )}{" "}
+                        -{" "}
+                        {getPlayerNameById(
+                          tournament.players,
+                          sortedPlayers[1].id
+                        )}
+                      </Typography>
+                    </Box>
+                  </Box>
+                );
+              })}
           </Box>
         );
       })}
