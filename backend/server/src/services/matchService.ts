be8907631d5eb31095554d543272a971275357db
Alex <nalex1211@gmail.com>
2024-11-17 17:53:07 +0200

    lint issues repair

--- backend/server/src/services/matchService.ts
+++ backend/server/src/services/matchService.ts
@@ -975,7 +975,7 @@ export class MatchService {
   } | null> {
     const tournament = await TournamentModel.findById(tournamentId).exec();
 
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    // eslint-disable-next-line @typescript-eslint/prefer-optional-chain
     if (tournament == null || tournament.teams == null) {
       return null;
     }
